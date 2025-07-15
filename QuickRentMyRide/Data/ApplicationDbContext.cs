b2dcefddae2fb579fd572a3c7e8bf65355029365
Ashokkumar Natheesan <UT010842@unicomtic.com>
2025-07-15 12:26:42 +0530

    Create Folders

---
+++ QuickRentMyRide/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuickRentMyRide.Models;
+namespace QuickRentMyRide.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
